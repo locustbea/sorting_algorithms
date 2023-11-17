42696681fa3b56f898201a351a78d01d79d03632
Austasty <obuesiogonna2@gmail.com>
2023-11-17 09:29:50 +0100

    added quick sort files file by olalekan

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,36 +1,37 @@
 #include "sort.h"
-#include <stdio.h>
 
 /**
- * bubble_sort - sorts an array of integers in decending order
+ * bubble_sort - implementation of bubble sort algorithm
+ * @array: array of integers
+ * @size: size of the array of integers
  *
- * @array: The array of integer
- * @size: The size of the array
- *
- * Return: void
+ * Return: nothing
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i, j, temp, counter, passages;
 
-	size_t i;
-	int tmp, sorted;
-
-	if (!array || size == 0)
+	counter = 0;
+	passages = 0;
+	if (array == NULL)
 		return;
-
-	do {
-		sorted = 1;
-		for (i = 0; i < size - 1; i++)
+	if (size <= 1)
+		return;
+	while (passages < size)
+	{
+		for (i = 1, j = 0; i < size; i++, j++)
 		{
-			if (array[i] > array[i + 1])
+			if (array[j] > array[i])
 			{
-				sorted = 0;
-				tmp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = tmp;
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				counter++;
 				print_array(array, size);
 			}
 		}
-
-	} while (sorted == 0);
+		passages++;
+		if (counter == 0)
+			return;
+	}
 }
