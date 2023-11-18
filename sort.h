1d8764297b1b720bab812efaaec4a9fa0b88a7a1
locustbea <locostbea@yahoo.com>
2023-11-18 11:31:03 -0800

    sort header

--- sort.h
+++ sort.h
@@ -10,7 +10,9 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
+
 typedef struct listint_s
+
 {
 	const int n;
 	struct listint_s *prev;
